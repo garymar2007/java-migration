2e882c71dc01efa71f9a261bdacc54cc0343e2c2
Gary Ma <gary.ma@csgi.com>
2023-02-13 19:12:00 +0200

    Migrating Java 8 to Java 11

--- src/main/java/com/linkedin/hsports/services/RecommendationService.java
+++ src/main/java/com/linkedin/hsports/services/RecommendationService.java
@@ -1,14 +1,13 @@
 package com.linkedin.hsports.services;
 
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.linkedin.hsports.domain.Product;
-
-import sun.misc.BASE64Decoder;
 
 @Service
 public class RecommendationService {
@@ -25,10 +24,10 @@ public class RecommendationService {
 
 		List<Product> results = new ArrayList<>();
 		List<Product> products = this.productService.getProducts();
-		BASE64Decoder decoder = new BASE64Decoder();
 
 		try {
-			String key = new String(decoder.decodeBuffer(this.config.getLicenseKey()));
+			String key = new String(Base64.getDecoder().decode(this.config.getLicenseKey()));
+		
 			System.out.println(key);
 			if (licenseKey.equals(key)) {
 				results = products.subList(products.indexOf(product) + 1, this.config.getRecommendationNumber() - 1);
